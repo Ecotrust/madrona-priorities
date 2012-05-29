c08d63bb28a1b31c3bbb9bdb14308c82643dd7bb
Matthew Perry <mperry@ecotrust.org>
2012-05-29 09:49:59 -0700

    watched script to rebuild media

---
+++ nplcc/watch.sh
@@ -0,0 +1,7 @@
+while true
+    do
+        inotifywait -r -e modify -e move -e create -e delete ../media ~/src/madrona/madrona/media/ | while read line
+            do
+                python manage.py install_media
+            done
+    done

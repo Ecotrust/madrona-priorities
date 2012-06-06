e50705adf10003aff59cfc5ffd5ae854b46739bb
Matthew Perry <mperry@ecotrust.org>
2012-06-06 11:22:28 -0700

    admin to show content for flatblocks

--- nplcc/seak/admin.py
+++ nplcc/seak/admin.py
@@ -9,3 +9,12 @@ admin.site.register(Cost)
 admin.site.register(PuVsCf)
 admin.site.register(PuVsCost)
 
+# Override flatblock admin
+from flatblocks.models import FlatBlock
+class FlatBlockAdmin(admin.ModelAdmin):
+    ordering = ['slug', ]
+    list_display = ('slug', 'content')
+    search_fields = ('slug', 'content')
+
+admin.site.unregister(FlatBlock)
+admin.site.register(FlatBlock, FlatBlockAdmin)

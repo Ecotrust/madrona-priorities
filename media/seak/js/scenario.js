2d280148e9b88ae00b762bfcaccf7b2892a1bfe2
Matthew Perry <mperry@ecotrust.org>
2012-06-11 17:56:06 -0700

    Get geography selection working again.

--- media/seak/js/scenario.js
+++ media/seak/js/scenario.js
@@ -274,6 +274,7 @@ function scenariosViewModel() {
     selectGeographyControl.unselectAll();
     selectGeographyControl.deactivate();
     pu_layer.styleMap.styles.default.defaultStyle.display = "none";
+    pu_layer.redraw();
     self.showScenarioFormPanel(false);
     self.showScenarioList(true);
   };

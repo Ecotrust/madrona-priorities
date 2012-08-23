c3d7e335d33eea8c537a76d405d20420951ca818
Matthew Perry <mperry@ecotrust.org>
2012-08-22 17:28:15 -0700

    correct order of args in import

--- nplcc/import.sh
+++ nplcc/import.sh
@@ -1,4 +1,4 @@
  python manage.py import_planning_units \
-    ~/projects/nplcc/data_20120822/H1K_Values20120821_Merc.shp \
-    ~/projects/nplcc/data_20120822/NPLCC_metrics20120822.xls \
-    ~/projects/nplcc/data_20120822/H1K_Values20120821_Merc_Simp.shp
+    data/data_20120822/H1K_Values20120821_Merc_Simp.shp \
+    data/data_20120822/NPLCC_metrics20120822.xls \
+    data/data_20120822/H1K_Values20120821_Merc.shp

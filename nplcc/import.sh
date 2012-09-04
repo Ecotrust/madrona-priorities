47b380cfbbe63d9957a64d029292052948d2affc
Matthew Perry <mperry@ecotrust.org>
2012-09-04 16:02:01 -0700

    import new xls

--- nplcc/import.sh
+++ nplcc/import.sh
@@ -1,4 +1,4 @@
  python manage.py import_planning_units \
     data/data_20120822/H1K_Values20120821_Merc_Simp.shp \
-    data/data_20120822/NPLCC_metrics20120822.xls \
+    data/data_20120822/NPLCC_metrics20120904_mp.xls \
     data/data_20120822/H1K_Values20120821_Merc.shp

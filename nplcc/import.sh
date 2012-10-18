5f3d94f62ceb1d930661b6a4562e79b31a558ea0
Matthew Perry <mperry@ecotrust.org>
2012-10-18 13:33:33 -0700

    use simplified version in import

--- nplcc/import.sh
+++ nplcc/import.sh
@@ -1,4 +1,6 @@
 DATA_DIR=data/data_10172012
 python manage.py import_planning_units \
-    $DATA_DIR/HUC5_final_11_merc.shp \
-    $DATA_DIR/JUNIPERBLM_metrics20121017_MARXAN.xls
+    $DATA_DIR/HUC5_final_11_merc_simple250m.shp \
+    $DATA_DIR/JUNIPERBLM_metrics20121017_MARXAN.xls \
+    $DATA_DIR/HUC5_final_11_merc.shp
+

45b5b907a0e8612ec2fdcb2bd93ec64ac5a66793
Matthew Perry <mperry@ecotrust.org>
2012-11-07 10:00:51 -0800

    test datasets

--- priorities/import.sh
+++ priorities/import.sh
@@ -1,6 +1,9 @@
-DATA_DIR=~/projects/juniper/data_10172012
-python manage.py import_planning_units \
-    $DATA_DIR/HUC5_final_11_merc_simple250m.shp \
-    $DATA_DIR/JUNIPERBLM_metrics20121017_MARXAN.xls \
-    $DATA_DIR/HUC5_final_11_merc.shp
+# DATA_DIR=../testdata
+# python manage.py import_planning_units \
+#    $DATA_DIR/planning_units.shp \
+#    $DATA_DIR/metrics.xls
 
+DATA_DIR=../testdata
+python manage.py import_planning_units \
+    $DATA_DIR/planning_units.shp \
+    $DATA_DIR/metrics.xls

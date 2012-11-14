f8affe1e6869bd4f46dc4e69295e8028cc660abe
Matthew Perry <mperry@ecotrust.org>
2012-11-13 16:38:38 -0800

    Check for acronyms when deslugifying

--- priorities/seak/templatetags/deslug.py
+++ priorities/seak/templatetags/deslug.py
@@ -2,8 +2,18 @@ from django import template
 
 register = template.Library()
 
+def make_acronym(w):
+    acronyms = [
+        'BLM',
+    ]
+    if w.upper() in acronyms:
+        return w.upper()
+    else:
+        return w
+
 @register.filter 
 def deslug(value): 
     words = value.split('-')
     words[0] = words[0].capitalize()
+    words = [make_acronym(w) for w in words]
     return ' '.join(words)

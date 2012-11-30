28f03a173387dc9398d50f0a076580131d0c6fcf
Matthew Perry <mperry@ecotrust.org>
2012-11-30 12:07:37 -0800

    precaching tiles, docs. One step closer to #17

--- priorities/seak/management/commands/precache.py
+++ priorities/seak/management/commands/precache.py
@@ -1,13 +1,43 @@
 import os
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from seak.models import Cost, ConservationFeature
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         from seak.views import planning_units_geojson
         from django.test.client import RequestFactory
+        from madrona.layer_manager.views import get_json 
 
         print "Caching the planning_units.geojson response..."
         request = RequestFactory().get('/seak/planning_units.geojson')
         planning_units_geojson(request)
 
+        print "Caching the layer_manager response..."
+        request = RequestFactory().get('/layer_manager/layers.json')
+        get_json(request)
+
+        print "Caching some tiles..."
+        sz = settings.JS_OPTS['start_zoom']
+        nz = settings.JS_OPTS['num_levels']
+
+        zooms = [str(x) for x in range(sz,sz+nz)]
+
+        ex = [str(x) for x in settings.JS_OPTS['extent']]
+        extent = ' '.join([ex[1], ex[0], ex[3], ex[2]])
+
+        layers = [ 
+            ('utfgrid', 'json'),
+            ('planning_units', 'png'),
+        ]
+
+        layers.extend([(x.dbf_fieldname, 'png') for x in Cost.objects.all()])
+        layers.extend([(x.dbf_fieldname, 'png') for x in ConservationFeature.objects.all()])
+        print layers
+
+        for z in zooms:
+            for layer in layers:
+                cmd = "tilestache-seed.py -c ../tile_config/tiles.cfg -l %s -e %s -b %s %s" % (layer[0], layer[1], extent, z)
+                print cmd
+                os.popen(cmd)

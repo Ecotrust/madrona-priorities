0efaa5c1170b60daeff57ec81ac385c98052ff06
Matthew Perry <mperry@ecotrust.org>
2013-02-14 16:21:55 -0800

    starting vagrant/puppet/fabric deploy system

---
+++ fabfile.py
@@ -0,0 +1,103 @@
+from fabric.api import *
+
+vars = {
+    'app_dir': '/usr/local/apps/madrona-priorities/priorities',
+    'venv': '/usr/local/venv/priorities'
+}
+
+env.forward_agent = True
+env.key_filename = '~/.vagrant.d/insecure_private_key'
+
+
+def dev():
+    """ Use development server settings """
+    servers = ['vagrant@127.0.0.1:2222']
+    env.hosts = servers
+    return servers
+
+
+def prod():
+    """ Use production server settings """
+    servers = []
+    env.hosts = servers
+    return servers
+
+
+def test():
+    """ Use test server settings """
+    servers = []
+    env.hosts = servers
+    return servers
+
+
+def all():
+    """ Use all servers """
+    env.hosts = dev() + prod() + test()
+
+
+def _install_requirements():
+    run('cd %(app_dir)s && %(venv)s/bin/pip install -r ../requirements.txt' % vars)
+
+
+def _install_django():
+    run('cd %(app_dir)s && %(venv)s/bin/python manage.py syncdb --noinput && \
+                           %(venv)s/bin/python manage.py migrate --noinput && \
+                           %(venv)s/bin/python manage.py install_media -a && \
+                           %(venv)s/bin/python manage.py enable_sharing --all && \
+                           %(venv)s/bin/python manage.py install_cleangeometry' % vars)
+
+
+def create_superuser():
+    """ Create the django superuser (interactive!) """
+    run('cd %(app_dir)s && %(venv)s/bin/python manage.py createsuperuser' % vars)
+
+
+def import_data():
+    """ Fetches and installs data fixtures (WARNING: 5+GB of data; hence not checking fixtures into the repo) """
+    run('cd %(app_dir)s && %(venv)s/bin/python manage.py import_data' % vars)
+
+
+def init():
+    """ Initialize the application """
+    _install_requirements()
+    _install_django()
+    _install_marxan()
+    _load_fixtures()
+
+
+def runserver():
+    """ Run the django dev server on port 8000 """
+    run('cd %(app_dir)s && %(venv)s/bin/python manage.py runserver 0.0.0.0:8000' % vars)
+
+
+def update():
+    """ Sync with master git repo """
+    run('cd %(app_dir)s && git fetch && git merge origin/master' % vars)
+    init()
+
+
+def _install_marxan():
+    run('echo "######################### INSTALL MARXAN????"')
+
+
+def _load_fixtures():
+    run('cd %(app_dir)s && %(venv)s/bin/python manage.py loaddata fixtures/flatblocks.json && \
+                           %(venv)s/bin/python manage.py loaddata fixtures/project_base_layers.json' % vars)
+
+# TODO
+# figure out line b/t puppet and fabric duties
+# run test suite
+# run selenium
+# a "bootstrap_puppet" command to ssh into an arbitrary box, transfer files, set things up and run puppet
+#  .. basically a vagrant up for non virtualbox servers
+
+# TODO celeryd under supervisor control
+"""
+(lot)vagrant@precise32:/usr/local/apps/land_owner_tools$ sudo supervisorctl reload
+Restarted supervisord
+(lot)vagrant@precise32:/usr/local/apps/land_owner_tools$ sudo supervisorctl status
+celeryd                          STARTING
+(lot)vagrant@precise32:/usr/local/apps/land_owner_tools$ sudo supervisorctl restart celeryd
+celeryd: stopped
+celeryd: started
+"""
---
+++ util/puppet/manifests/files/settings_vagrant.py
@@ -0,0 +1,27 @@
+SECRET_KEY = 'secret'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'priorities',
+        'USER': 'vagrant',
+    }
+}
+
+# This should be a local folder created for use with the install_media command 
+MEDIA_ROOT = '/usr/local/apps/madrona-priorities/mediaroot/'
+MEDIA_URL = 'http://localhost:8000/media/'
+STATIC_URL = 'http://localhost:8000/media/'
+
+POSTGIS_TEMPLATE='template1'
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG 
+
+ADMINS = (
+        ('Madrona', 'madrona@ecotrust.org')
+        ) 
+
+import logging
+logging.getLogger('django.db.backends').setLevel(logging.ERROR)
+import os
+LOG_FILE = os.path.join(os.path.dirname(__file__),'..','seak.log')

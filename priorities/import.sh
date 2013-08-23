a4892719261dc2a47f952bc10c836e2d8112d118
Matthew Perry <mperry@ecotrust.org>
2013-08-23 09:18:02 -0700

    updates

--- priorities/import.sh
+++ priorities/import.sh
@@ -1,6 +1,6 @@
-DATA_DIR=~/projects/BLM_Aquatics/data_20121210
+DATA_DIR=../data/local/ToolInputs20130822
 python manage.py import_planning_units \
-    $DATA_DIR/HUC10_BLM_20121210_simple.shp \
-    $DATA_DIR/BLM_metrics20121210.xls \
-    $DATA_DIR/HUC10_BLM_20121210.shp
+    $DATA_DIR/HUC8_20130206_merc_simple.shp \
+    $DATA_DIR/USFWS_metrics20130822.xls \
+    $DATA_DIR/HUC8_20130206_merc.shp
 
